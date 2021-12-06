0ab4f2457f496f4662d56ac75568bef9d7966856
mohamed <chourabi247@gmail.com>
2021-12-06 22:19:09 +0100

    Update OfferItem.tsx

--- src/components/OfferItem.tsx
+++ src/components/OfferItem.tsx
@@ -4,9 +4,21 @@ import { Offer } from "../models/offer";
 const OfferItem: React.FC<Partial<Offer>> = ({ type, value, sliceValue }) => {
   return (
     <>
-      <p>{type}</p>
-      <p>{value}</p>
-      <p>{sliceValue}</p>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h3>La meilleure offre applicable est:</h3>
+            <p>
+              <span>Type de l'offre: </span>
+              {type}
+            </p>
+            <p>
+              <span>La valeur de la remise =</span> {value}
+            </p>
+            <p>{sliceValue}</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
